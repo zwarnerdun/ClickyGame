4db558f24ca5c600ee00993d2ad73f168197b489
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-09-05 19:15:45 -0400

    got client side to show and saved my pictures for the game

---
+++ my-app/src/components/Home.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div> 
+            <nav className="navbar">
+                <ul>
+                    <li className="game">
+                        <a href="/">Rocko's Clicky Game</a>
+                    </li>
+                    <li class>Click an image to begin!</li>
+                    <li>
+                        "Score:"
+                        "0"
+                        " | Top Score "
+                        "0"
+                    </li>
+                </ul> 
+            </nav>
+            <header class="header">
+                <h1>Rocko's Modern Life</h1>
+                <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+            </header>
+            <main class="container">
+
+            </main>
+            <footer class="footer">
+                <div class="bottom">
+                    
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Home;
