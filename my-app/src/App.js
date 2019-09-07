9f8ca771700ef7399c98d1ca0c7196c0a7a6b91a
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-09-07 09:46:59 -0400

    added friends in a json instead of png or jpg, added colomns, rows and now configuring my app to work through my app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import Home from './components/Home'
+import React, {Component} from 'react';
+import Home from "./components/Home"
+import Title from "./components/Title/Title";
+import FriendCard from "./components/Friends/Friendscard";
+import rockofriends from "./rockosfriends.json";
+import Container from "./Container";
+import Row from "./Row";
+import colomn from "./colomn";
+import "./App.css"
 
-
-function App() {
-  return <Home />
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
+class App extends Component {
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topscore: 0,
+    rightwrong: "",
+    clicked: []
+  };
+
+
 
 export default App;
