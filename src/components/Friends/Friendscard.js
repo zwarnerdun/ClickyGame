47204c854ba9a5548ecd02b8387950f114ab8b73
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-09-09 18:16:22 -0400

    redoing folder

---
+++ src/components/Friends/Friendscard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Friend.css";
+
+const FriendCard = props => (
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
+
+
+export default FriendCard;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Home.css";
+
+function Home (props){
+    return(
+        <nav>
+            <ul>
+                <li className="brand animated lightSpeedIn">
+                    <a href="/ClickyGame">{props.title}</a>
+                </li>
+
+                <li id="ci">{props.correctincorrect}</li>
+
+                <li id="score">Score: {props.score}</li>
+
+                <li id="top">Top Score: {props.topScore}</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
